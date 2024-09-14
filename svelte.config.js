044ea39d6ad4e2657bae7b1adf182c25f6cbe9eb
Myslitel666 <tretyakov_02@bk.ru>
2024-09-14 15:21:46 +0500

    i

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from "@sveltejs/adapter-static";
-/** @type {import('@sveltejs/kit').Config} */
+
+const dev = process.env.NODE_ENV === 'development';
+
 const config = {
 	kit: {
 		adapter: adapter(),
