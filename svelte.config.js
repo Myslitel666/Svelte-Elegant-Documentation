cd66a0daa16e787385322fdbd3f2b835dc0fb88f
Myslitel666 <tretyakov_02@bk.ru>
2024-09-14 15:20:44 +0500

    path.base added

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@ import adapter from "@sveltejs/adapter-static";
 const config = {
 	kit: {
 		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/Svelte-Elegant-Docs'  // Укажите здесь базовый путь
+		  }
 	},
 };
 
